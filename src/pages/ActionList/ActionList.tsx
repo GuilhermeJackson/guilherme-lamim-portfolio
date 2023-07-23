35bffecf6ef55e0818f095afc7e2e0a2ad25f0e4
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-07-23 02:53:01 -0300

    created action list

--- src/pages/ActionList/ActionList.tsx
+++ src/pages/ActionList/ActionList.tsx
@@ -1,11 +1,10 @@
 import './ActionList.less'
 import TitleSubtitleText from '../../components/TitleSubtitleText/TitleSubtitleText'
-import { TbMessageDollar, TbDeviceDesktop, TbBrandWhatsapp } from 'react-icons/tb'
-import { TfiGallery } from 'react-icons/tfi'
+import { TbMessageDollar, TbDeviceDesktop, TbBrandWhatsapp, TbBrandGithub, TbBrandLinkedin } from 'react-icons/tb'
 
 function ActionList() {
   const whats = 'https://api.whatsapp.com/send?phone=5547991056721&text=Ol%C3%A1!%20Gostaria%20de%20conversar%20sobre%20os%20servi%C3%A7os%20de%20desenvolvimento.%0APodemos%20discutir%20mais%20detalhes?';
-
+  const iconSize = 32;
   return (
     <div className='al-container'>
       <div className='al-list-action'>
@@ -13,31 +12,45 @@ function ActionList() {
         <div className='al-list-buttons'>
           <a href={whats} target="_blank">
             <div className='al-item-button' >
-              <div className='al-icon-button'><TbMessageDollar size={32} /></div>
+              <div className='al-icon-button'><TbMessageDollar size={iconSize} /></div>
               <div className='al-title-button'><span>solicitar um orçamento</span></div>
             </div>
           </a>
           <a href='/home'>
             <div className='al-item-button'>
-              <div className='al-icon-button'><TbDeviceDesktop size={32} /></div>
+              <div className='al-icon-button'><TbDeviceDesktop size={iconSize} /></div>
               <div className='al-title-button'><span>conheça meu site</span></div>
             </div>
           </a>
-          <a href={whats} target="_blank">
+          {/* <a href={whats} target="_blank">
             <div className='al-item-button'>
-              <div className='al-icon-button'><TfiGallery size={32} /></div>
+              <div className='al-icon-button'><TfiGallery size={iconSize} /></div>
               <div className='al-title-button'><span>confira meu portfólio</span></div>
             </div>
-          </a>
+          </a> */}
           <a href={whats} target="_blank">
             <div className='al-item-button'>
-              <div className='al-icon-button'><TbBrandWhatsapp size={32} /></div>
+              <div className='al-icon-button'><TbBrandWhatsapp size={iconSize} /></div>
               <div className='al-title-button'><span>tire suas dúvidas</span></div>
             </div>
           </a>
         </div>
         <div className='al-social-media'>
-
+          <div className='al-social-circle'>
+            <a href='https://github.com/GuilhermeJackson' target='__blank'>
+              <TbBrandGithub size={iconSize} />
+            </a>
+          </div>
+          <div className='al-social-circle'>
+            <a href='https://www.linkedin.com/in/guilhermelamim/' target='__blank'>
+              <TbBrandLinkedin size={iconSize} />
+            </a>
+          </div>
+          <div className='al-social-circle'>
+            <a href={whats} target='__blank'>
+              <TbBrandWhatsapp size={iconSize} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
