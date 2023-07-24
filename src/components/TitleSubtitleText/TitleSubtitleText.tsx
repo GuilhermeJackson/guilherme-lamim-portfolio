d3f44757e320f6ee9551480dc93c10d359badfd0
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-07-24 13:44:23 -0300

    chenge event title

--- src/components/TitleSubtitleText/TitleSubtitleText.tsx
+++ src/components/TitleSubtitleText/TitleSubtitleText.tsx
@@ -5,17 +5,15 @@ function TitleSubtitleText() {
     <div className='al-title-wrapper'>
       <div className='al-title'>
         <h2 >
-          <span className="al-span-first-name" data-aos="fade-left" data-aos-anchor="al-title"
-            data-aos-offset="10"
-            data-aos-delay="100">
+          <span className="al-span-first-name">
             Guilherme
           </span>
-          <span className="al-span-last-name" data-aos="fade-right" data-aos-anchor="al-span-first-name">
+          <span className="al-span-last-name" >
             Lamim
           </span>
         </h2>
       </div>
-      <div className='al-subtitle' data-aos="fade-left" data-aos-anchor="al-span-last-name">
+      <div className='al-subtitle' >
         <span><strong>Desenvolvedor front-end</strong></span>
       </div>
 
