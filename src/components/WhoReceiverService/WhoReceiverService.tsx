21cdc17a161266ae7f9240311481a86fc5f11d86
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-07-28 00:20:17 -0300

    w/p

--- src/components/WhoReceiverService/WhoReceiverService.tsx
+++ src/components/WhoReceiverService/WhoReceiverService.tsx
@@ -18,19 +18,22 @@ function WhoReceiverService({ title, listReceiver }: WhoReceiverServiceProps) {
       <div className='who-receiver-list-card'>
         {listReceiver.map((item, index) => {
           return <div key={index} className='who-receiver-list-item'>
-            <img src={item.img} />
-            <div className='who-receiver-list-title-subtitle'>
-              <div className='who-receiver-list-title'>
-                <h2>{item.title}</h2>
-              </div>
-              <div className='who-receiver-list-subtitle'>
-                <span>{item.description}</span>
-              </div>
-              <div className="who-receiver-list-buttons">
-                {item.github === '' ? '' : <a href={item.github} target="_blank">GitHub</a>}
-                {item.site === '' ? '' : <a href={item.site} target="_blank">Site</a>}
+            <div>
+              <img src={item.img} />
+              <div className='who-receiver-list-title-subtitle'>
+                <div className='who-receiver-list-title'>
+                  <h2>{item.title}</h2>
+                </div>
+                <div className='who-receiver-list-subtitle'>
+                  <span>{item.description}</span>
+                </div>
               </div>
             </div>
+            <div className="who-receiver-list-buttons">
+              {item.github === '' ? '' : <a href={item.github} target="_blank">GitHub</a>}
+              {item.site === '' ? '' : <a href={item.site} target="_blank">Site</a>}
+            </div>
+
           </div>
         })}
       </div>
