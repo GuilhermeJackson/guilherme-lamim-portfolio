0e83db8986165ad4ebf1aec2cd4784fc7845211c
Guilherme Lamim <guilherme.lamim96@gmail.com>
2023-08-02 22:50:36 -0300

    inserted vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.less'
 import AppRouter from './routes/AppRouter.tsx'
 import Layout from './pages/Layout/Layout.tsx'
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Layout children={<AppRouter />} />
